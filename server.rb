3172b731bc8d59f742ae33c48e5129bddd281ced
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-12 16:43:21 -0400

    add route to Favourites Page

--- server.rb
+++ server.rb
@@ -8,6 +8,13 @@ get '/portfolio' do
   erb :gallery
 end
 
-get '/aboutme' do
-  erb :aboutme
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['camping', 'motorcycles', 'socialist literature', 'films']
+  erb :about_me
+end
+
+get '/favourites' do
+  @fav_links = ['https://www.pottermore.com/', 'https://en.wikipedia.org/', 'https://www.reddit.com/', 'https://www.thebeaverton.com/', 'https://www.iamafoodblog.com/']
+    erb :favourites
 end
